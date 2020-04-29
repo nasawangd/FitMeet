f270b4ead74d62df9f5178e342c038157c25ac68
Derek Nasawang <nasawangd@gmail.com>
2020-04-29 14:36:42 -0700

    Created Login and Registration page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class Header extends Component{
+  render() {
+    return (
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            FitMeet
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+    );
+  }
+
 }
 
+
+class Body extends Component{
+  render() {
+    return (
+      <p>
+        Body render
+      </p>
+    )
+  }
+}
+
+
+
+
+class App extends Component{
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Body />
+      
+      </div>
+    );
+  }
+}
 export default App;
